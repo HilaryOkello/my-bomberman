98e9c164d67caf9394abdb6cc80d0c75aaece546
hiombima <ombimahillary52@gmail.com>
2025-02-18 15:41:00 +0300

    refactor: update handleKeys in GameController class

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import gameBoard from "./gameBoard.js";
 import gameController from "./activatePlayer.js";
-import { placeBomb } from "./bombPlacement.js";
 import {checkAllEnemiesCollision, updateEnemyPosition, updateEnemy } from "./enemyPlacement.js"
 
 // Add this to script.js
@@ -50,33 +49,8 @@ function renderGame() {
     });
   }
 }
-// Initialize when the DOM is fully loaded
+  // Initialize when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
   gameBoard.initializeBoard();
   requestAnimationFrame(gameLoop);
-  document.addEventListener("keydown", (event) => {
-    if (event.key === 'p') {
-      if (gameController.isPaused) {
-        gameController.resumeGame();
-      } else {
-        gameController.pauseGame();
-      }
-    }
-
-
-    if (gameController.isPlaying && !gameController.isPaused) {
-      switch (event.key) {
-        case "ArrowUp":
-        case "ArrowDown":
-        case "ArrowLeft":
-        case "ArrowRight":
-          // Handle movement
-          break;
-        case " ":
-          placeBomb();
-          break;
-      }
-    }
-  });
 });
-
